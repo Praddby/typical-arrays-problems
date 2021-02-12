45499876d02a6cb7d25f67d617bc717d57fcf4a5
Yury Bialou <Pradd.by@gmail.com>
2021-02-12 09:29:17 +0300

    feat: add functions min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 
 exports.min = function min (array) {
-  return 0;
+  if ( typeof(array) === 'undefined' || array.length === 0) return 0;
+  return (array.sort((a,b)=>a-b))[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if ( typeof(array) === 'undefined' || array.length === 0) return 0;
+  return (array.sort((a,b)=>b-a))[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if ( typeof(array) === 'undefined' || array.length === 0) return 0;
+  let result = array.reduce((sum, curr) => sum + curr, 0);
+	return parseFloat((result / array.length).toFixed(2));
 }
